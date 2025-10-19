5d3a586b17cd8e8f920d0949a908e212a173354d
nktuyen <nktuyen@hotmail.com>
2025-10-20 01:37:32 +0700

    Remove unneccessary files

--- FileOperation/MainForm.cs
+++ FileOperation/MainForm.cs
@@ -1124,6 +1124,7 @@ namespace FileOperation
                         }
                         catch (System.Exception ex)
                         {
+                            Debug.Print(ex.Message);
                             continue;
                         }
                     }
